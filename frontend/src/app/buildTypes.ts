95121ff86e0b2034d4a612fd3932a477e75754cd
Harry-Panajoti <98226996+Harry-Panajoti@users.noreply.github.com>
2023-02-11 17:31:41 -0500

    added Build,CenterGravity,superClimb Types.ts

---
+++ frontend/src/app/buildTypes.ts
@@ -0,0 +1,11 @@
+export class MatchScoutingL1 {
+    buildTypeID: number;
+    buildType: string
+
+constructor(
+    buildTypeID: number,
+    buildType: string
+){
+    this.buildTypeID = buildTypeID
+    this.buildType = buildType
+}}
---
+++ frontend/src/app/centerGravityTypes.ts
@@ -0,0 +1,11 @@
+export class MatchScoutingL1 {
+    centerGravityTypeID: number;
+    centerGravityType: string
+
+constructor(
+    centerGravityTypeID: number,
+    centerGravityType: string
+){
+    this.centerGravityTypeID = centerGravityTypeID
+    this.centerGravityType = centerGravityType
+}}
