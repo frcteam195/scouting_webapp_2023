bdb1ebda68de2d56030e0b9d46b583e8582b90da
DavidC1234567890 <124469088+DavidC1234567890@users.noreply.github.com>
2023-02-17 19:25:49 -0500

    Update allianceStation.ts

---
+++ frontend/src/app/allianceStation.ts
@@ -0,0 +1,11 @@
+export class AllianceStation {
+    allianceStationID: number;
+    allianceStation: string;
+
+constructor(
+    allianceStationID: number,
+    allianceStation: string,
+ ){
+    this.allianceStationID=allianceStationID;
+    this.allianceStation=allianceStation;
+}}
