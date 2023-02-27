96e62f001a56686e319e99af0fbea4abeabe94bb
ddrex <44214161+ddrex@users.noreply.github.com>
2023-02-27 17:35:50 -0500

    Debug/Admin changes.

--- frontend/src/app/modules/debug/debug.component.ts
+++ frontend/src/app/modules/debug/debug.component.ts
@@ -1,8 +1,10 @@
+import { PitScouting } from './../../pitScouting';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { MatchScoutingL2 } from 'src/app/matchScoutingL2';
 import { ApiService } from 'src/app/services/api.service';
 import { MatchScoutingL1 } from '../../matchScoutingL1';
+
 
 
 
@@ -22,6 +24,13 @@ export interface MatchHeader {
   scouterID: number;
 }
 
+export interface PitHeader {
+  team: string;
+  eventID: number;
+  scoutingStatus: number;
+  scouterID: number;
+}
+
 
 
 @Component({
@@ -37,13 +46,18 @@ export class DebugComponent implements OnInit {
   apiMatchL2: MatchScoutingL2[] = [];
   apiFilter1: MatchHeader[] = [];
 
+  apiPit: PitScouting[] = [];
+  apiPitFilter: PitHeader[] = [];
+
   dumpMemory: string = '';
   memoryType: string = '';
+  display: number = 1;
 
   memoryTypes: MemoryTypes[] = [{memoryTypeID:"MatchL1",memoryType:"Match Records-Level 1"},
-                                {memoryTypeID:"MatchL2",memoryType:"Match Records-Level 2"},
                                 {memoryTypeID:"Level1",memoryType:"Stored Records-Level 1"},
-                                {memoryTypeID:"Level2",memoryType:"Stored Records-Level 2"}];
+                                {memoryTypeID:"MatchL2",memoryType:"Match Records-Level 2"},
+                                {memoryTypeID:"Level2",memoryType:"Stored Records-Level 2"},
+                                {memoryTypeID:"Pit",memoryType:"Pit Records"}];
 
   constructor(private apiService: ApiService, private formBuilder: FormBuilder) { 
 
@@ -52,6 +66,8 @@ export class DebugComponent implements OnInit {
     this.apiLevel2 = [];
     this.apiMatchL2 = [];
     this.apiFilter1 = [];
+    this.apiPit = [];
+    this.apiPitFilter = [];
     
     this.apiService.MatchL1Replay.subscribe(match => {
       this.apiMatchL1 = match;
@@ -68,6 +84,13 @@ export class DebugComponent implements OnInit {
     this.apiService.StoredL2Replay.subscribe(match => {
       this.apiLevel2 = match;
     });
+
+    this.apiService.PitReplay.subscribe(team => {
+      this.apiPit = team;
+
+      // Sort by Team Number
+      this.apiPit.sort((a, b) => Number(a.team) - Number(b.team));
+    });
     
 
     this.regenerateFilter();
@@ -75,7 +98,7 @@ export class DebugComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    // this.regenerateFilter();
+    this.regenerateFilter();
   }
 
   select(type: string) {
@@ -92,7 +115,9 @@ export class DebugComponent implements OnInit {
     if (this.memoryType == "Level1" && this.apiLevel1) {
 
       this.apiFilter1 = [];
-      
+      this.apiPitFilter = [];
+      this.display = 1;
+
       // Filter
       for (const m of this.apiLevel1) {
         this.apiFilter1.push({matchScoutingID:m.matchScoutingID,
@@ -107,7 +132,9 @@ export class DebugComponent implements OnInit {
     } else if (this.memoryType == "MatchL1" && this.apiMatchL1) {
 
       this.apiFilter1 = [];
-      
+      this.apiPitFilter = [];
+      this.display = 1;
+
       // Filter
       for (const m of this.apiMatchL1) {
         this.apiFilter1.push({matchScoutingID:m.matchScoutingID,
@@ -122,7 +149,9 @@ export class DebugComponent implements OnInit {
     } else     if (this.memoryType == "Level2" && this.apiLevel2) {
 
       this.apiFilter1 = [];
-      
+      this.apiPitFilter = [];
+      this.display = 1;
+
       // Filter
       for (const m of this.apiLevel2) {
         this.apiFilter1.push({matchScoutingID:m.matchScoutingL2ID,
@@ -137,7 +166,9 @@ export class DebugComponent implements OnInit {
     } else if (this.memoryType == "MatchL2" && this.apiMatchL2) {
 
       this.apiFilter1 = [];
-      
+      this.apiPitFilter = [];
+      this.display = 1;
+
       // Filter
       for (const m of this.apiMatchL2) {
         this.apiFilter1.push({matchScoutingID:m.matchScoutingL2ID,
@@ -148,9 +179,23 @@ export class DebugComponent implements OnInit {
                               team:m.team,
                               scoutingStatus:m.scoutingStatus,
                               scouterID:m.scouterID});
+      }
+    } else if (this.memoryType == "Pit" && this.apiPit) {
+
+      this.apiFilter1 = [];
+      this.apiPitFilter = [];
+      this.display = 2;
+      
+      // Filter
+      for (const t of this.apiPit) {
+        this.apiPitFilter.push({team:t.team,
+                              eventID:t.eventID,
+                              scoutingStatus:t.scoutingStatus,
+                              scouterID:t.scouterID});
       } 
     } else {
       this.apiFilter1 = [];
+      this.apiPitFilter = [];
     }
   }
 
